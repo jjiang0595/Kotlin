34434fe034921f80d006b2984fcbf3bb804dff90
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-03 22:17:16 -0500

    more datatypes

--- Fundamentals/KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ Fundamentals/KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -13,5 +13,15 @@ fun main() {
     // type int
     var myAge = 31
 
+    var isSunny: Boolean = true
+    isSunny = false
+
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+
     print("Hello " + myName)
 }
