bbdbb72d955aa26538fd935695daa320ca768adc
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-04 21:17:27 -0500

    member variables

--- Intro To Android Studio/MyFirstApp/app/src/main/java/com/example/myfirstapp/KotlinBasics.kt
+++ Intro To Android Studio/MyFirstApp/app/src/main/java/com/example/myfirstapp/KotlinBasics.kt
@@ -1,19 +1,35 @@
 package com.example.myfirstapp
 
 fun main() {
-    var denis = Person("Denis", "Panjuta")
+    var denis = Person("Denis", "Panjuta", 31)
+    denis.hobby = "to skateboard"
+    denis.age = 32
+    println("Denis is ${denis.age} years old")
     var john = Person()
     var johnPeterson = Person(lastName = "Peterson")
-
+    denis.stateHobby()
     val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
     val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
     val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
 }
 
 class Person constructor(firstName: String = "John", lastName: String = "Doe") {
+    var firstName: String? = null
+    var age: Int? = null
+    var hobby: String = "watch Netflix"
 
     init {
+        this.firstName = firstName
         println("Person created with first name $firstName and last name $lastName")
+    }
+
+    constructor(firstName: String = "John", lastName: String = "Doe", age: Int): this(firstName, lastName) {
+        this.age = age;
+        println("Person created with first name $firstName and last name $lastName with age $age")
+    }
+
+    fun stateHobby() {
+        println("${this.firstName}'s hobby is $hobby")
     }
 }
 
