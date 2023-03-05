818e0e9a053f8abd8f348a581cf1380abeafd2bd
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-04 20:43:41 -0500

    classes exercise

--- Intro To Android Studio/MyFirstApp/app/src/main/java/com/example/myfirstapp/KotlinBasics.kt
+++ Intro To Android Studio/MyFirstApp/app/src/main/java/com/example/myfirstapp/KotlinBasics.kt
@@ -4,11 +4,22 @@ fun main() {
     var denis = Person("Denis", "Panjuta")
     var john = Person()
     var johnPeterson = Person(lastName = "Peterson")
+
+    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
+    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
+    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
 }
 
 class Person constructor(firstName: String = "John", lastName: String = "Doe") {
 
     init {
         println("Person created with first name $firstName and last name $lastName")
+    }
+}
+
+class MobilePhone constructor(osName: String, brand: String, model: String) {
+
+    init {
+        println("OS Name: $osName brand: $brand model: $model")
     }
 }
