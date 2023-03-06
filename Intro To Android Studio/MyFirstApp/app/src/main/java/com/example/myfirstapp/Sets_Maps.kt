ca6bfe342e942def9c50f0eb32293ae5c4946a1c
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-05 19:54:09 -0500

    sets and maps

---
+++ Intro To Android Studio/MyFirstApp/app/src/main/java/com/example/myfirstapp/Sets_Maps.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val fruits = setOf("Orange", "Apple", "Mango", "Grape", "Apple")
+    print(fruits.toSortedSet())
+
+    val newFruits = fruits.toMutableList()
+    newFruits.add("Watermelon")
+    newFruits.add("Pear")
+
+    val daysOfTheWeek = mapOf(1 to "Monday", 2 to "Tuesday",  3 to "Wednesday")
+    
+}
