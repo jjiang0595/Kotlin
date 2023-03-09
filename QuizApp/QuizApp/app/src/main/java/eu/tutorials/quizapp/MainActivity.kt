ac6fa4fc31aec319298ee227a47cd16ce42d9e4b
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-09 14:26:49 -0500

    creating question model and question list

--- QuizApp/QuizApp/app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
+++ QuizApp/QuizApp/app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package eu.tutorials.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val btnStart : Button = findViewById(R.id.btn_start)
+        val etName : EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
